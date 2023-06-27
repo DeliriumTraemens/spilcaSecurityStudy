82f2fd26ceee3f8dd574a3d26381e53568f1fc37
Grigorich <oskinnick@gmail.com>
2023-06-27 19:32:44 +0300

    First commit Bean PasswordEncoder added

--- src/main/java/org/mykola/spilcaSecurityStudy/config/ProjectConfig.java
+++ src/main/java/org/mykola/spilcaSecurityStudy/config/ProjectConfig.java
@@ -2,15 +2,31 @@ package org.mykola.spilcaSecurityStudy.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+
+//        PasswordEncoder instance = NoOpPasswordEncoder.getInstance();
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
     public UserDetailsService userDetailsService(){
         var userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("Nick")
+                .password("12345")
+                .authorities("read")
+                .build();
+        userDetailsService.createUser(user);
         return userDetailsService;
     }
 
