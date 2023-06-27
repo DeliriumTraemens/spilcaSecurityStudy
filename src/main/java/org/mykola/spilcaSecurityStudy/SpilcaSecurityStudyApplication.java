cc8d66a1421aed13501a2fcba1d514f61a9fc117
Grigorich <oskinnick@gmail.com>
2023-06-27 17:52:04 +0300

    Initial commit with security

---
+++ src/main/java/org/mykola/spilcaSecurityStudy/SpilcaSecurityStudyApplication.java
@@ -0,0 +1,13 @@
+package org.mykola.spilcaSecurityStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpilcaSecurityStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpilcaSecurityStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mykola/spilcaSecurityStudy/SpilcaSecurityStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mykola.spilcaSecurityStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpilcaSecurityStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
