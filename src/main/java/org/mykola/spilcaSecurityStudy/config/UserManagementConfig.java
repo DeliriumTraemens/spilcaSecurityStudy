82765691b4439affb840e552ac340de175a67eac
Grigorich <oskinnick@gmail.com>
2023-06-30 13:57:24 +0300

    UserManagementConfig.java content is commented

--- src/main/java/org/mykola/spilcaSecurityStudy/config/UserManagementConfig.java
+++ src/main/java/org/mykola/spilcaSecurityStudy/config/UserManagementConfig.java
@@ -15,46 +15,46 @@ import org.springframework.security.web.authentication.AuthenticationFilter;
 public class UserManagementConfig {
 
 
-
-    @Bean
-    public UserDetailsService userDetailsService() {
 //
-
-
-        User.UserBuilder builder1 = User.withUsername("Bill");
-        UserDetails u1 = builder1
-                .password("123")
-                .authorities("read", "write")
-//                .passwordEncoder(p -> encode(p))//it is NOT A BEAN of PassEncoder!!!
-                .accountExpired(false)
-                .disabled(true)
-                .build();
-//=================
-        UserDetails u = User.withUsername("bill")
-                .password("12345")
-                .authorities("read", "write")
-                .accountExpired(false)
-                .disabled(true)
-                .build();
-
-        User.UserBuilder builder2 = User.withUserDetails(u);
-        UserDetails u2 = builder2.build();
-//================
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+////
 //
-        AuthenticationFilter af;
-        var userDetailsService = new InMemoryUserDetailsManager();
-        var user = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build();
-        userDetailsService.createUser(user);
-
-
-        return userDetailsService;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
+//
+//        User.UserBuilder builder1 = User.withUsername("Bill");
+//        UserDetails u1 = builder1
+//                .password("123")
+//                .authorities("read", "write")
+////                .passwordEncoder(p -> encode(p))//it is NOT A BEAN of PassEncoder!!!
+//                .accountExpired(false)
+//                .disabled(true)
+//                .build();
+////=================
+//        UserDetails u = User.withUsername("bill")
+//                .password("12345")
+//                .authorities("read", "write")
+//                .accountExpired(false)
+//                .disabled(true)
+//                .build();
+//
+//        User.UserBuilder builder2 = User.withUserDetails(u);
+//        UserDetails u2 = builder2.build();
+////================
+////
+//        AuthenticationFilter af;
+//        var userDetailsService = new InMemoryUserDetailsManager();
+//        var user = User.withUsername("john")
+//                .password("12345")
+//                .authorities("read")
+//                .build();
+//        userDetailsService.createUser(user);
+//
+//
+//        return userDetailsService;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
 }
