e6da8bb9ea75175e834bc65da52c9caf7b838783
Grigorich <oskinnick@gmail.com>
2023-07-03 13:44:43 +0300

    In ProjectConfig.java not working Bean UserDetails Service - cycle dependency

--- src/main/java/org/mykola/spilcaSecurityStudy/config/ProjectConfig.java
+++ src/main/java/org/mykola/spilcaSecurityStudy/config/ProjectConfig.java
@@ -3,11 +3,10 @@ package org.mykola.spilcaSecurityStudy.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -17,19 +16,33 @@ import javax.sql.DataSource;
 
 @Configuration
 //@EnableWebSecurity
-public class ProjectConfig{
+public class ProjectConfig extends WebSecurityConfigurerAdapter{
 
+    @Autowired
+    AuthenticationProvider customAuthenticationProvider; // Its wired to our CustomAuthProvider!!!
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth){
+        auth.authenticationProvider(customAuthenticationProvider);//Load our CustomAuthenticationProvider into config
+    }
+
+//Replacement from author
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
-        String usersByUsernameQuery =
-                "select username, password, enabled from spilcaSecurity.users where username = ?";
-        String authsByUserQuery =
-                "select username, authority from spilcaSecurity.authorities where username = ?";
-        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
-        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
-        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
-        return userDetailsManager;
+        return new JdbcUserDetailsManager(dataSource);
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        String usersByUsernameQuery =
+//                "select username, password, enabled from spilcaSecurity.users where username = ?";
+//        String authsByUserQuery =
+//                "select username, authority from spilcaSecurity.authorities where username = ?";
+//        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+//        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+//        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+//        return userDetailsManager;
+//    }
 
 
 
--- src/main/java/org/mykola/spilcaSecurityStudy/security/CustomAuthenticationProvider.java
+++ src/main/java/org/mykola/spilcaSecurityStudy/security/CustomAuthenticationProvider.java
@@ -2,8 +2,11 @@ package org.mykola.spilcaSecurityStudy.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -17,7 +20,15 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();//Password is contained in credentials!!!
+
+        UserDetails u = userDetailsService.loadUserByUsername(username);
+
+        if(passwordEncoder.matches(password,u.getPassword())){
+            return new UsernamePasswordAuthenticationToken(username,password,u.getAuthorities());
+        } else throw new BadCredentialsException("Something went wrong");
+
     }
 
     @Override
